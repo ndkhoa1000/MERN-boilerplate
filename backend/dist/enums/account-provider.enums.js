b9c5e7b7166e77aff159791b41ac607cedbe8a21
L8duck <ndkhoa1000@gmail.com>
2025-06-01 16:34:52 +0700

    fix: delete dist and update gitignore

--- backend/dist/enums/account-provider.enums.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProviderEnum = void 0;
-exports.ProviderEnum = {
-    GOOGLE: "GOOGLE",
-    FACEBOOK: "FACEBOOK",
-    GITHUB: "GITHUB",
-    EMAIL: "EMAIL"
-};
